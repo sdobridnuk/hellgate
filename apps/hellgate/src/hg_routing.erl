70a25f7d65d7f2aa31147c7de793fe630da89305
Roman Pushkov <pushkovroman@me.com>
2019-04-05 10:17:34 +0300

    add GetStatistics call to routing

--- apps/hellgate/src/hg_routing.erl
+++ apps/hellgate/src/hg_routing.erl
@@ -2,6 +2,7 @@
 
 -module(hg_routing).
 -include_lib("dmsl/include/dmsl_domain_thrift.hrl").
+-include_lib("fault_detector_proto/include/fd_proto_fault_detector_thrift.hrl").
 
 -export([choose/4]).
 -export([get_payments_terms/2]).
@@ -109,13 +110,14 @@ score_risk_coverage({_Provider, {_TerminalRef, Terminal}}, VS) ->
 
 collect_providers(Predestination, PaymentInstitution, VS, Revision, RejectContext) ->
     ProviderSelector = PaymentInstitution#domain_PaymentInstitution.providers,
-    ProviderRefs = reduce(provider, ProviderSelector, VS, Revision),
-
-    %% TODO: Use data from fault detector here
-    %% see score_providers_with_fault_detector/1
+    ProviderRefs0    = reduce(provider, ProviderSelector, VS, Revision),
+    ProviderRefs1    = ordsets:to_list(ProviderRefs0),
+    ScoredProviders  = score_providers_with_fault_detector(ProviderRefs1),
 
     {Providers, RejectReasons} = lists:foldl(
-        fun (ProviderRef, {Prvs, Reasons}) ->
+        %% TODO: use failure rate in some meaningful way
+        %% failure rate ranges from 0.0 to 1.0, 1.0 being a service that is completely unavailable
+        fun ({ProviderRef, _FailureRate}, {Prvs, Reasons}) ->
             try
                 P = acceptable_provider(Predestination, ProviderRef, VS, Revision),
                 {[P | Prvs], Reasons}
@@ -125,16 +127,22 @@ collect_providers(Predestination, PaymentInstitution, VS, Revision, RejectContex
             end
         end,
         {[], []},
-        ordsets:to_list(ProviderRefs)
+        ScoredProviders
     ),
     {Providers, RejectContext#{rejected_providers => RejectReasons}}.
 
-% score_providers_with_fault_detector([]) -> [];
-% score_providers_with_fault_detector([ProviderRef]) -> [ProviderRef];
-% score_providers_with_fault_detector(ProviderRefs) ->
-%     %% IDs    = [PR#domain_ProviderRef.id || PR <- ProviderRefs],
-%     %% Scores = hg_fault_detector_client:get_statistics(ProviderRefs),
-%     ProviderRefs.
+score_providers_with_fault_detector([]) -> [];
+score_providers_with_fault_detector([ProviderRef]) -> [{ProviderRef, 0}];
+score_providers_with_fault_detector(ProviderRefs) ->
+    IDs     = [PR#domain_ProviderRef.id || PR <- ProviderRefs],
+    FDStats = hg_fault_detector_client:get_statistics(IDs),
+    [get_provider_score_fail_rate(PR, FDStats) || PR <- ProviderRefs].
+
+get_provider_score_fail_rate(ProviderRef, FDStats) ->
+    case lists:keysearch(ProviderRef#domain_ProviderRef.id, 2, FDStats) of
+         {value, Stats} -> {ProviderRef, Stats#fault_detector_ServiceStatistics.failure_rate};
+         _              -> {ProviderRef, 0}
+    end.
 
 acceptable_provider(payment, ProviderRef, VS, Revision) ->
     Provider = #domain_Provider{
